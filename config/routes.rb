d6aca40dd39e553623c2ddee7e718390ac3f5269
hopegipson <gipson.hope@gmail.com>
2020-11-19 21:25:01 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  post 'add_to_cart', to: 'products#add', as: 'add_to_cart'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
